fa2647bc96176a6e37c392ad9ef6cde777dad6c5
Zikov Roman <beeline.mts@gmail.com>
2018-03-23 22:12:34 +0300

    upd
    update code

--- sample_db/sample_db.hpp
+++ sample_db/sample_db.hpp
@@ -4,36 +4,36 @@
 
 namespace sample_dbc {
 	
-	//users
 	struct value_users_get {
 		eosio::string user_email;
-        eosio::string user_uid;
+        	eosio::string user_uid;
 		eosio::string phone;
 		eosio::string password;
 		eosio::string status;
 		eosio::string rating;
-        account_name  owner;
+        	account_name  owner;
     };
 	
 	struct value_getuser_get {
 		eosio::string user_email;
 		eosio::string result_operation;
-        account_name  owner;
+        	account_name  owner;
     };
 	
 	struct value_deluser_get {
 		eosio::string user_email;
 		eosio::string result_operation;
-        account_name  owner;
+        	account_name  owner;
     };
 	
 	struct PACKED(by_user){
-        eosio::string user_email;
-		eosio::string user_uid;
+        	eosio::string user_email;
+        	eosio::string user_uid;
 		eosio::string phone;
 		eosio::string password;
 		eosio::string status;
-        eosio::string rating;
+		eosio::string rating;
+        	account_name  owner;
     };
 	
 }
